7845c250ed5cf9651a522f806976fe601528b3bb
RJB <113512137+1RJB@users.noreply.github.com>
2024-06-24 15:01:09 +0800

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,80 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+import { Container, AppBar, Toolbar, Typography, Box, Button } from '@mui/material';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import MyTheme from './themes/MyTheme';
+import Rewards from './pages/Rewards';
+import AddReward from './pages/AddReward';
+import EditReward from './pages/EditReward';
+import MyForm from './pages/MyForm';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import http from './http';
+import UserContext from './contexts/UserContext';
+
+function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    if (localStorage.getItem("accessToken")) {
+      http.get('/user/auth').then((res) => {
+        setUser(res.data.user);
+      });
+    }
+  }, []);
+
+  const logout = () => {
+    localStorage.clear();
+    window.location = "/";
+  };
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      <Router>
+        <ThemeProvider theme={MyTheme}>
+          <AppBar position="static" className="AppBar">
+            <Container>
+              <Toolbar disableGutters={true}>
+                <Link to="/">
+                  <Typography variant="h6" component="div">
+                    Green Neighbourhood
+                  </Typography>
+                </Link>
+                <Link to="/rewards" ><Typography>Rewards</Typography></Link>
+                <Box sx={{ flexGrow: 1 }}></Box>
+                {user && (
+                  <>
+                    <Typography>{user.name}</Typography>
+                    <Button onClick={logout}>Logout</Button>
+                  </>
+                )
+                }
+                {!user && (
+                  <>
+                    <Link to="/register" ><Typography>Register</Typography></Link>
+                    <Link to="/login" ><Typography>Login</Typography></Link>
+                  </>
+                )}
+              </Toolbar>
+            </Container>
+          </AppBar>
+
+          <Container>
+            <Routes>
+              <Route path={"/"} element={<Rewards />} />
+              <Route path={"/rewards"} element={<Rewards />} />
+              <Route path={"/addreward"} element={<AddReward />} />
+              <Route path={"/editreward/:id"} element={<EditReward />} />
+              <Route path={"/register"} element={<Register />} />
+              <Route path={"/login"} element={<Login />} />
+              <Route path={"/form"} element={<MyForm />} />
+            </Routes>
+          </Container>
+        </ThemeProvider>
+      </Router>
+    </UserContext.Provider>
+  );
+}
+
+export default App;
