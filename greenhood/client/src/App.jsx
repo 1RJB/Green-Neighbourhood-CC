8037f9ddaa009d6f7ea014a2ed02b424975e829d
modermix <lesong4025@gmail.com>
2024-07-24 11:45:46 +0800

    useless changes
    useless

--- greenhood/client/src/App.jsx
+++ greenhood/client/src/App.jsx
@@ -1,25 +1,75 @@
-// App.jsx
+import './App.css';
+import { useState, useEffect } from 'react';
+import { Container, AppBar, Toolbar, Typography, Box, Button } from '@mui/material';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import MyTheme from './themes/MyTheme';
 import Register from './pages/Register';
 import Login from './pages/Login';
-import React from "react";
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Container } from "react-bootstrap";
-import Header from "./header";
-
+import http from './http';
+import UserContext from './contexts/UserContext';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    if (localStorage.getItem("accessToken")) {
+      http.get('/user/auth').then((res) => {
+        setUser(res.data.user);
+      });
+    }
+  }, []);
+
+  const logout = () => {
+    localStorage.clear();
+    window.location = "/";
+  };
+
   return (
-    <Router>
-      <Header />
-      <Container className="mt-4">
-        <Routes>
-          <Route path="/" element={<div>Home</div>} />
-          <Route path={"/register"} element={<Register />} />
-          <Route path={"/login"} element={<Login />} />
-        </Routes>
-      </Container>
-    </Router>
+    <UserContext.Provider value={{ user, setUser }}>
+      <Router>
+        <ThemeProvider theme={MyTheme}>
+          <AppBar position="static" className="AppBar">
+            <Container>
+              <Toolbar disableGutters={true}>
+                <Link to="/">
+                  <Typography variant="h6" component="div">
+                    Learning
+                  </Typography>
+                </Link>
+                <Link to="/tutorials" ><Typography>Tutorials</Typography></Link>
+                <Box sx={{ flexGrow: 1 }}></Box>
+                {user && (
+                  <>
+                    <Typography>{user.name}</Typography>
+                    <Button onClick={logout}>Logout</Button>
+                  </>
+                )
+                }
+                {!user && (
+                  <>
+                    <Link to="/register" ><Typography>Register</Typography></Link>
+                    <Link to="/login" ><Typography>Login</Typography></Link>
+                  </>
+                )}
+              </Toolbar>
+            </Container>
+          </AppBar>
+
+          <Container>
+            <Routes>
+              <Route path={"/"} element={<Tutorials />} />
+              <Route path={""} element={<Tutorials />} />
+              <Route path={""} element={<AddTutorial />} />
+              <Route path={""} element={<EditTutorial />} />
+              <Route path={"/register"} element={<Register />} />
+              <Route path={"/login"} element={<Login />} />
+              <Route path={"/form"} element={<MyForm />} />
+            </Routes>
+          </Container>
+        </ThemeProvider>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
