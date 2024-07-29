5b42595b0973c7b7ecb395f870d057c45bfc0416
modermix <lesong4025@gmail.com>
2024-07-29 15:12:49 +0800

    profileupdate
    profileupdate

--- Customer/client/src/pages/userprofile.jsx
+++ Customer/client/src/pages/userprofile.jsx
@@ -1,30 +1,25 @@
-// src/pages/UserProfile.jsx
 import React, { useContext } from 'react';
-import { Box, Typography, Avatar } from '@mui/material';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 import UserContext from '../contexts/UserContext';
 
 function UserProfile() {
     const { user } = useContext(UserContext);
 
     return (
-        <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <Avatar src={user.pfp} alt={user.firstName} sx={{ width: 100, height: 100 }} />
-            <Typography variant="h5" sx={{ my: 2 }}>
-                {user.firstName} {user.lastName}
-            </Typography>
-            <Typography variant="body1" sx={{ my: 2 }}>
-                Email: {user.email}
-            </Typography>
-            <Typography variant="body1" sx={{ my: 2 }}>
-                Gender: {user.gender}
-            </Typography>
-            <Typography variant="body1" sx={{ my: 2 }}>
-                Birthday: {user.birthday}
-            </Typography>
-            <Typography variant="body1" sx={{ my: 2 }}>
-                User Type: {user.usertype}
-            </Typography>
-        </Box>
+        <Container style={{ marginTop: '50px' }}>
+            <Row className="justify-content-center">
+                <Col md={6} className="text-center">
+                    <Image src={user.pfp} alt={user.firstName} roundedCircle style={{ width: '150px', height: '150px' }} />
+                </Col>
+                <Col md={6}>
+                    <h5 className="my-3">{user.firstName} {user.lastName}</h5>
+                    <p className="my-2"><strong>Email:</strong> {user.email}</p>
+                    <p className="my-2"><strong>Gender:</strong> {user.gender}</p>
+                    <p className="my-2"><strong>Birthday:</strong> {user.birthday}</p>
+                    <p className="my-2"><strong>User Type:</strong> {user.usertype}</p>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
