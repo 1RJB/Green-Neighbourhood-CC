81b5848092f6d7feb094a62533d2c7ec150208e9
Ryan <ryanb0717@gmail.com>
2024-08-09 22:11:29 +0800

    Update User.js - updated User association with Achievement to use composite key

--- server/models/User.js
+++ server/models/User.js
@@ -1,57 +1,58 @@
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define(
-      "User",
-      {
-          firstName: {
-              type: DataTypes.STRING(25),
-              allowNull: false,
-          },
-          lastName: {
-              type: DataTypes.STRING(25),
-              allowNull: false,
-          },
-          email: {
-              type: DataTypes.STRING(50),
-              allowNull: false,
-          },
-          gender: {
-              type: DataTypes.STRING(10),
-              allowNull: false,
-          },
-          birthday: {
-              type: DataTypes.DATEONLY,
-              allowNull: false,
-          },
-          password: {
-              type: DataTypes.STRING(100),
-              allowNull: false,
-          },
-          usertype: {
-              type: DataTypes.STRING(10),
-              allowNull: false,
-              defaultValue: "user",
-          },
-          points: {
-              type: DataTypes.INTEGER,
-              allowNull: false,
-              defaultValue: 0,
-          }
-      },
-      {
-          tableName: "users",
-          timestamps: true,
-      }
-  );
+    const User = sequelize.define(
+        "User",
+        {
+            firstName: {
+                type: DataTypes.STRING(25),
+                allowNull: false,
+            },
+            lastName: {
+                type: DataTypes.STRING(25),
+                allowNull: false,
+            },
+            email: {
+                type: DataTypes.STRING(50),
+                allowNull: false,
+            },
+            gender: {
+                type: DataTypes.STRING(10),
+                allowNull: false,
+            },
+            birthday: {
+                type: DataTypes.DATEONLY,
+                allowNull: false,
+            },
+            password: {
+                type: DataTypes.STRING(100),
+                allowNull: false,
+            },
+            usertype: {
+                type: DataTypes.STRING(10),
+                allowNull: false,
+                defaultValue: "user",
+            },
+            points: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                defaultValue: 0,
+            }
+        },
+        {
+            tableName: "users",
+            timestamps: true,
+        }
+    );
 
-  User.associate = (models) => {
-      User.hasMany(models.Redemption, { foreignKey: 'userId', as: 'redemptions' });
-      User.belongsToMany(models.Achievement, {
-        through: 'UserAchievements',
-        as: 'achievements',
-        foreignKey: 'achievementId'
-    });
-  };
-  
+    User.associate = (models) => {
+        User.hasMany(models.Redemption, { foreignKey: 'userId', as: 'redemptions' });
+        User.belongsToMany(models.Achievement, {
+          through: 'UserAchievements',
+          as: 'achievements',
+          foreignKey: 'userId',
+          otherKey: 'achievementId'
+        });
+      };
+      
 
-  return User;
+    return User;
 };
