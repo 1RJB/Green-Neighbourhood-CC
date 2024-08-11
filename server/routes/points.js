d1d1caf3586b099f577b56b48d4f72f09a992a74
Ryan <ryanb0717@gmail.com>
2024-08-11 22:04:03 +0800

    Update points.js - modified ways to get points

--- server/routes/points.js
+++ server/routes/points.js
@@ -5,9 +5,12 @@ const { User } = require('../models');
 // This endpoint will return information on how to earn points
 router.get('/points-info', async (req, res) => {
     try {
-        // Fetch point info from the database or define it statically for now
+        // Point info, way to earn more points
         const pointsInfo = [
             { title: 'Referral', description: 'Refer a friend to get points', points: 5000, actionText: 'Refer Now' },
+            { title: 'Events', description: 'Take part in events', points: 10000, actionText: 'Participate Now' },
+            { title: 'Volunteers', description: 'Volunteer for a cause', points: 15000, actionText: 'Volunteer Now' },
+            { title: 'Quizzes', description: 'Take quizzes to earn points', points: 5000, actionText: 'Take Quiz Now' },
             // Add more ways to earn points here
         ];
         res.json(pointsInfo);
